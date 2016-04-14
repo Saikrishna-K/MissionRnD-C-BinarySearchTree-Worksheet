481be5b9de61ead58eba44029c3d73cd3eafaea1
Saikrishna-K <Saikrishna-K@missionrnd.com>
2016-04-14 17:06:42 +0530

    Submit Commit

--- src/SortedArraytoBST.cpp
+++ src/SortedArraytoBST.cpp
@@ -34,8 +34,55 @@ struct node{
 };
 
 
-struct node * convert_array_to_bst(int *arr, int len){
-	
+struct node *createNode(int *arr, int index)
+{
+
+	struct node* temp = (struct node*)calloc(1,sizeof(struct node));
+	temp->data = arr[index];
+	temp->left = NULL;
+	temp->right = NULL;
+	return temp;
+}
+
+
+
+
+struct node* addNode(int *arr, int left, int right)
+{
+	if (left <= right)
+	{
+		struct node* temp = NULL;
+		int mid = (left + right) / 2;
+		temp = createNode(arr, mid);
+		
+		temp->left = addNode(arr, left, mid - 1);
+		temp->right = addNode(arr, mid + 1, right);
+
+		return temp;
+	}
 	return NULL;
 }
 
+
+
+
+
+
+struct node * convert_array_to_bst(int *arr, int len){
+
+	if (arr == NULL) return NULL;
+
+	else
+	{
+		struct node*temp = NULL;
+		int mid = (0 + len - 1) / 2;
+		temp = createNode(arr, mid);
+		temp->left = addNode(arr, 0, mid - 1);
+		temp->right = addNode(arr, mid + 1, len - 1);
+		
+		return temp;
+
+	}
+
+
+}
